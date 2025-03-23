8fcd93d0a888bf473fe6a93a90c4e011b22a865b
shubhhmalik <131964323+shubhhmalik@users.noreply.github.com>
2025-03-23 06:47:21 +0530

    Update script.js

--- static/script.js
+++ static/script.js
@@ -1,7 +1,10 @@
 document.getElementById('urlForm').addEventListener('submit', async (e) => {
     e.preventDefault();
+    
+    //Get the long URL from user
     const longUrl = document.getElementById('longUrl').value;
 
+    //POST request to shorten the URL
     const response = await fetch('/shorten', {
         method: 'POST',
         headers: {
@@ -12,10 +15,12 @@ document.getElementById('urlForm').addEventListener('submit', async (e) => {
 
     const data = await response.json();
     if (data.short_url) {
+        //Display the short URL
         document.getElementById('result').innerHTML = `
             <p>Short URL: <a href="${data.short_url}" target="_blank">${data.short_url}</a></p>
         `;
     } else {
+        // for an error, show this error message
         document.getElementById('result').innerHTML = `<p>Error: ${data.error}</p>`;
     }
 });
